744f9dcfda88bbc4da3826c0392144f9d769c758
Nayeem Shaik <nayeemshaiksfdc@gmailcom>
2025-06-09 12:12:42 +0530

    golang course

---
+++ mygolang-lco/02variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("This is Variable")
+	var username string = "Aydin"
+	fmt.Println(username)
+	fmt.Printf("Variable of type %T \n", username)
+}
