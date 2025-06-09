a43be8a76197d0472533354df3ca800a1fcf254a
nayeemshaikdev <nayeemshaikdev@gmail.com>
2025-06-09 12:29:03 +0530

    test

--- mygolang-lco/02variables/main.go
+++ mygolang-lco/02variables/main.go
@@ -6,5 +6,5 @@ func main()  {
 	fmt.Println("This is Variable")
 	var username string = "Aydin"
 	fmt.Println(username)
-	fmt.Printf("Variable of type %T \n", username)
+	fmt.Printf("Variable of type %T \n", username) 
 }
