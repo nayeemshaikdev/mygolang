e28c751c48c43ff2bf6d184dacff5a90b8c88d19
nayeemshaikdev <nayeemshaikdev@gmail.com>
2025-06-09 22:48:20 +0530

    test commit

--- mygolang-lco/02variables/main.go
+++ mygolang-lco/02variables/main.go
@@ -6,5 +6,9 @@ func main()  {
 	fmt.Println("This is Variable")
 	var username string = "Aydin"
 	fmt.Println(username)
-	fmt.Printf("Variable of type %T \n", username) 
+	fmt.Printf("Variable of type %T \n", username)
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable of type %T \n", anotherVariable) 
 }
