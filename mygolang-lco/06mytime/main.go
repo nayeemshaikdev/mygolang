2ab2fc070fa6d364f415f411580522e83c5067da
nayeemshaikdev <nayeemshaikdev@gmail.com>
2025-06-11 11:23:51 +0530

    mytime

---
+++ mygolang-lco/06mytime/main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+	"time"
+)
+
+func main()  {
+	fmt.Println("Welcome to my time")
+
+	presentTime :=time.Now()
+
+	fmt.Println(presentTime)
+
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2022, time.July, 05, 5, 30, 0, 0, time.UTC)
+
+	fmt.Println("Aydin Birthday : ", createdDate)
+	fmt.Println("Aydin Birthday : ", createdDate.Format("01-02-2006 Monday"))
+}
